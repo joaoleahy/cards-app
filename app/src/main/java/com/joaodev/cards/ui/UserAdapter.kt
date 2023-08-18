96470141411a336b5e52f7d8d9564de38a29f2f5
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-08-18 18:50:43 -0300

    style(UserAdapter): increase spacing between cards for better readability

--- app/src/main/java/com/joaodev/cards/ui/UserAdapter.kt
+++ app/src/main/java/com/joaodev/cards/ui/UserAdapter.kt
@@ -3,50 +3,36 @@ package com.joaodev.cards.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.joaodev.cards.R
 import com.joaodev.cards.data.models.User
 
-class UserAdapter(private val userGroups: List<List<User>>) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+class UserAdapter(private val users: List<User>) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user_card, parent, false)
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_user_card, parent, false)
         return UserViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        val usersInGroup = userGroups[position]
-        holder.bind(usersInGroup)
+        val user: User = users[position]
+        holder.bind(user)
     }
 
     override fun getItemCount(): Int {
-        return userGroups.size
+        return users.size
     }
 
-    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val userContainer: LinearLayout = itemView.findViewById(R.id.userLinearLayout)
+    inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+        private val ageTextView: TextView = itemView.findViewById(R.id.ageTextView)
+        private val emailTextView: TextView = itemView.findViewById(R.id.emailTextView)
 
-        fun bind(users: List<User>) {
-            userContainer.removeAllViews()
-
-            for (user in users) {
-                val userCard = LayoutInflater.from(itemView.context)
-                    .inflate(R.layout.item_user_card, userContainer, false)
-
-                val nameTextView: TextView = userCard.findViewById(R.id.nameTextView)
-                val ageTextView: TextView = userCard.findViewById(R.id.ageTextView)
-                val emailTextView: TextView = userCard.findViewById(R.id.emailTextView)
-
-                nameTextView.text = user.name
-                ageTextView.text = user.age.toString()
-                emailTextView.text = user.email
-
-                userContainer.addView(userCard)
-            }
+        fun bind(user: User) {
+            nameTextView.text = user.name
+            ageTextView.text = user.age.toString()
+            emailTextView.text = user.email
         }
     }
 }
-
-
