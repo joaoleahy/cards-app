22df8f08ffe7fcbd11343129ed7ec2772f40a0a9
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-08-17 22:44:01 -0300

    feat(MainActivity): implement MainActivity UI and API integration

---
+++ app/src/main/java/com/joaodev/cards/ui/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.joaodev.cards.ui
+
+import UserAdapter
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.joaodev.cards.R
+import com.joaodev.cards.data.services.UserResponse
+import com.joaodev.cards.data.services.clients.ApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        userAdapter = UserAdapter(emptyList())
+        recyclerView.adapter = userAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        val call = ApiClient.apiService.getUsers()
+        call.enqueue(object : Callback<UserResponse> {
+            override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
+                if (response.isSuccessful) {
+                    val users = response.body()?.users ?: emptyList()
+                    userAdapter = UserAdapter(users)
+                    recyclerView.adapter = userAdapter
+                }
+            }
+
+            override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+
+            }
+        })
+    }
+}
